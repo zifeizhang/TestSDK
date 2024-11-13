211a088ef7316a286c08ad3256d6907aa4a60187
zhangzifei <1967036155@qq.com>
2024-11-13 14:37:21 +0800

    sdk 提交

---
+++ CusFrameWork.framework/Headers/CusFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  CusFrameWork.h
+//  CusFrameWork
+//
+//  Created by zzf on 2024/11/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CusFrameWork.
+FOUNDATION_EXPORT double CusFrameWorkVersionNumber;
+
+//! Project version string for CusFrameWork.
+FOUNDATION_EXPORT const unsigned char CusFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CusFrameWork/PublicHeader.h>
+
+
+#import <CusFrameWork/CusNetWorkManager.h>
---
+++ CusFrameWork.framework/Headers/CusNetWorkManager.h
@@ -0,0 +1,18 @@
+//
+//  CusNetWorkManager.h
+//  CusFrameWork
+//
+//  Created by zzf on 2024/11/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CusNetWorkManager : NSObject
+
+- (void)connect;
+
+@end
+
+NS_ASSUME_NONNULL_END
